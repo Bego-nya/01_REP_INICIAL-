720cc0d74073dd7bdc9d51525e4b1be8a3b3a2d4
Begonya <begonyatebar@gmail.com>
2025-10-14 17:44:32 +0200

    Subo todo el proyecto

---
+++ src/HolaTrancos.java
@@ -0,0 +1,5 @@
+public class HolaTrancos{
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hola mundo!");
+    }
+}
---
+++ src/adiós/Adiós.java
@@ -0,0 +1,7 @@
+package adiós;
+
+public class Adiós {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Adiós Trancos!");
+    }
+} 
